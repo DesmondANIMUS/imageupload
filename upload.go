d3cc8c325209be93fc662d632d650b9290ca2046
Gerifield <gergely.radics1@ibm.com>
2018-10-04 08:55:20 +0200

    Revert "Revert "Add some tests""
    This reverts commit fec383aa780c7188bbeb888bd6b956d1e0e63b75.

--- upload.go
+++ upload.go
@@ -6,7 +6,7 @@ import (
 	"image/gif"
 	"image/jpeg"
 	"image/png"
-	"mime/multipart"
+	"io"
 	"net/http"
 	"os"
 	"strings"
@@ -16,6 +16,14 @@ import (
 
 var extMap map[string]int
 
+var fs fileSystem = osFS{}
+
+var ErrFileNotSupported = errors.New("file is not an image")
+
+func init() {
+	initExtMap()
+}
+
 // UploadFile function is a simple helper function that uploads and saves an image on the server
 // Params:
 // r: to get the picture from multi-part form using key "get_picture"
@@ -23,7 +31,6 @@ var extMap map[string]int
 // ID: unique string ID for the image
 // size: to resize the image, the function will keep the aspect ratio intact
 func UploadFile(r *http.Request, location string, ID string, size uint) (string, error) {
-	initExtMap()
 	var path string
 	file, hdr, err := r.FormFile("get_picture")
 	if err != nil {
@@ -42,15 +49,21 @@ func UploadFile(r *http.Request, location string, ID string, size uint) (string,
 }
 
 // SaveFile function helps in uploading the profile picture of user
-func saveFile(src multipart.File, location, ID, ext string, size uint) (string, error) {
+func saveFile(src io.Reader, location, ID, ext string, size uint) (string, error) {
 	name := ID + ".jpg"
 	path := "." + location + name
 	var img image.Image
 	var op jpeg.Options
 	var err error
 	op.Quality = 50
+	var err error
 
-	switch extMap[ext] {
+	e, ok := extMap[ext]
+	if !ok {
+		return "", ErrFileNotSupported
+	}
+
+	switch e {
 	case JPG:
 		img, err = decodeJPG(src, size)
 		if err != nil {
@@ -66,11 +79,9 @@ func saveFile(src multipart.File, location, ID, ext string, size uint) (string, 
 		if err != nil {
 			return "", err
 		}
-	default:
-		return "", errors.New("file is not an image")
 	}
 
-	dst, err := os.Create(path)
+	dst, err := fs.Create(path)
 	if err != nil {
 		return "", err
 	}
@@ -85,21 +96,21 @@ func saveFile(src multipart.File, location, ID, ext string, size uint) (string, 
 }
 
 // DecodeJPG function decodes JPG image
-func decodeJPG(src multipart.File, size uint) (image.Image, error) {
+func decodeJPG(src io.Reader, size uint) (image.Image, error) {
 	img, err := jpeg.Decode(src)
 	img = resize.Resize(size, 0, img, resize.Lanczos3)
 	return img, err
 }
 
 // DecodePNG function decodes PNG image
-func decodePNG(src multipart.File, size uint) (image.Image, error) {
+func decodePNG(src io.Reader, size uint) (image.Image, error) {
 	img, err := png.Decode(src)
 	img = resize.Resize(size, 0, img, resize.Lanczos3)
 	return img, err
 }
 
 // DecodeGIF function decodes GIF image
-func decodeGIF(src multipart.File, size uint) (image.Image, error) {
+func decodeGIF(src io.Reader, size uint) (image.Image, error) {
 	img, err := gif.Decode(src)
 	img = resize.Resize(size, 0, img, resize.Lanczos3)
 	return img, err
@@ -136,3 +147,17 @@ func reverse(txt string) string {
 
 	return string(result)
 }
+
+type fileSystem interface {
+	Create(name string) (file, error)
+}
+
+type file interface {
+	io.Closer
+	io.Writer
+}
+
+// osFS implements fileSystem using the local disk.
+type osFS struct{}
+
+func (osFS) Create(name string) (file, error) { return os.Create(name) }
